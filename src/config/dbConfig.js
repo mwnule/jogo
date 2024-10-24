288d55824b2e58622c52c4ccdd5f2a8fb652262b
mwnule~ <emanueledeoliveirasilveira@gmail.com>
2024-10-24 18:32:46 -0300

    .env

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async() => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/banco")
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`)
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB', error);
     }
